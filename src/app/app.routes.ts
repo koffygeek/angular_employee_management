ebd55f0598f7131516e3a45c56f159a7ba045f3e
Yvan Grebe <yvan.grebe@gmail.com>
2025-07-17 00:57:49 +0100

    prerequisities & components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Layout } from './components/layout/layout';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Employee } from './components/employee/employee';
+import { Leave } from './components/leave/leave';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: Login,
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'employee',
+        component: Employee,
+      },
+      {
+        path: 'leave',
+        component: Leave,
+      },
+    ],
+  },
+];
